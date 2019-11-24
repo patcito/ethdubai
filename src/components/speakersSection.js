705a9529da3cfa4ceae68f4cbee971cc6039aeee
Horacio Herrera <hi@horacioh.com>
2019-11-24 17:11:00 +0100

    speakers section component

---
+++ src/components/speakersSection.js
@@ -0,0 +1,218 @@
+import React from 'react'
+import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Modal } from 'react-bootstrap'
+import ReactMarkdown from 'react-markdown'
+
+export default function SpeakersSection({ speakers }) {
+  console.log('TCL: SpeakersSection -> speakers', speakers)
+  const [show, setShow] = React.useState(false)
+  const [speakerProps, setSpeakerProps] = React.useState({
+    name: '',
+    twitter: '',
+    github: '',
+    url: '',
+    shortBio: '',
+    bio: '',
+    avatarUrl: '',
+    localFile: {
+      childImageSharp: {
+        fluid: null,
+      },
+    },
+  })
+
+  const data = useStaticQuery(graphql`
+    {
+      head2: file(relativePath: { eq: "head-2.png" }) {
+        childImageSharp {
+          fixed(width: 80, height: 80) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      speaker_placeholder: file(relativePath: { eq: "you-round.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      pt: file(relativePath: { eq: "pt.png" }) {
+        childImageSharp {
+          fixed(width: 32, height: 32) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      pi: file(relativePath: { eq: "pi.png" }) {
+        childImageSharp {
+          fixed(width: 32, height: 32) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      pl: file(relativePath: { eq: "pl.png" }) {
+        childImageSharp {
+          fixed(width: 32, height: 32) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+    }
+  `)
+  console.log('TCL: SpeakersSection -> data', data)
+
+  return (
+    <section class="speaker" id="speakers">
+      <div class="container">
+        <div class="headings">
+          {data.head2.childImageSharp && (
+            <Img fixed={data.head2.childImageSharp.fixed} />
+          )}
+          <h2>Our Speakers</h2>
+          <p>
+            Stay tuned for some awesome speakers announcements soon as well as
+            our call for paper.
+          </p>
+        </div>
+        <div class="speaker_profile">
+          <div class="row">
+            {speakers.length > 0 &&
+              speakers.map(speaker => {
+                return (
+                  <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="speaker_box left_box">
+                      <div class="profile_image">
+                        <Img fluid={speaker.localFile.childImageSharp.fluid} />
+                      </div>
+                      <div class="profile_contnet">
+                        <h3 class="speaker-name">{speaker.name}</h3>
+                        <p class="speaker-bio" bio-full={speaker.bio}>
+                          {speaker.shortBio}
+                        </p>
+                        <ul>
+                          <li>
+                            <a
+                              href={`https://twitter.com/${speaker.twitter}`}
+                              class="icon-social-button"
+                            >
+                              <i class="fa fa-twitter icon-twitter"></i>
+                              <span />
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              href={`https://github.com/${speaker.github}`}
+                              class="icon-social-button"
+                            >
+                              <i class="fa fa-github icon-github"></i>
+                              <span />
+                            </a>
+                          </li>
+                          {speaker.url ? (
+                            <li>
+                              <a
+                                href="https://ivesvh.com/"
+                                class="icon-social-button"
+                              >
+                                <i class="fa fa-link icon-link"></i>
+                              </a>
+                            </li>
+                          ) : null}
+                        </ul>
+                      </div>
+                      <a
+                        href="#"
+                        class="add_icon show-speaker"
+                        data-toggle="modal"
+                        data-target="#speaker_popup"
+                        onClick={e => {
+                          e.preventDefault()
+                          setShow(true)
+                          setSpeakerProps(speaker)
+                        }}
+                      >
+                        +
+                      </a>
+                    </div>
+                  </div>
+                )
+              })}
+
+            <div class="col-md-3 col-sm-4 col-xs-12">
+              <div class="speaker_box right_box">
+                <div class="profile_image">
+                  <Img fluid={data.speaker_placeholder.childImageSharp.fluid} />
+                </div>
+                <div class="profile_contnet">
+                  <h3 class="speaker-name">You?</h3>
+                  <p class="speaker-bio">
+                    Stay tuned for more awesome speakers and our CFP!{' '}
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSejydjRdhyxE5sbzRqT93aHhx0PosforW88yZdem7HejNl-yA/viewform">
+                      Subscribe here to not miss new announcements
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="get_your_ticket">
+            <a href="#tickets">Get Your Ticket</a>
+          </div>
+        </div>
+      </div>
+      <Modal show={show} onHide={() => setShow(false)} id="speaker_popup">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                onClick={() => setShow(false)}
+              >
+                &times;
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="icon_box">
+                <ul>
+                  <li>
+                    <a
+                      href={`https://twitter.com/${speakerProps.twitter}`}
+                      class="speaker-twitter-modal"
+                    >
+                      <Img fixed={data.pt.childImageSharp.fixed} />
+                    </a>
+                    <a
+                      href={`https://github.com/${speakerProps.github}`}
+                      class="speaker-github-modal"
+                    >
+                      <Img fixed={data.pi.childImageSharp.fixed} />
+                    </a>
+                    <a href={speakerProps.url} class="speaker-url-modal">
+                      <Img fixed={data.pl.childImageSharp.fixed} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="speaker_popup_profile">
+                <div style={{ width: 200, margin: '0 auto' }}>
+                  <Img fluid={speakerProps.localFile.childImageSharp.fluid} />
+                </div>
+                <div class="popup_profile_content">
+                  <h3 class="speaker-name-modal">{speakerProps.name}</h3>
+                  <h5 class="speaker-bio-modal">{speakerProps.shortBio}</h5>
+                  <div class="speaker-bio-full-modal">
+                    <ReactMarkdown source={speakerProps.bio} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ import ReactMarkdown from 'react-markdown'
 import Hero from 'components/hero'
 import { request } from 'graphql-request'
 import ScheduleSection from 'components/scheduleSection'
+import SpeakersSection from 'components/speakersSection'
 
 const BlogIndex = ({ data, location }) => {
   //TODO: separate logic into components
@@ -24,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
   const [isFrench, setIsFrench] = useState(false)
   const [faq, setFaq] = useState(null)
   const [showSponsor, setShowSponsor] = useState(false)
-  const [show, setShow] = useState(false)
+
   const [schedule, setSchedule] = useState(
     data.eventlama.events[0].groupedSchedule
   )
@@ -35,20 +36,6 @@ const BlogIndex = ({ data, location }) => {
     jobUrl: '',
     description: '',
     level: '',
-  })
-  const [speakerProps, setSpeakerProps] = useState({
-    name: '',
-    twitter: '',
-    github: '',
-    url: '',
-    shortBio: '',
-    bio: '',
-    avatarUrl: '',
-    localFile: {
-      childImageSharp: {
-        fluid: null,
-      },
-    },
   })
 
   const clientQuery = `{
@@ -191,170 +178,7 @@ const BlogIndex = ({ data, location }) => {
         </div>
         <Caroussel />
       </section>
-      <a id="speakers"></a>
-      <section class="speaker" id="speaker">
-        <div class="container">
-          <div class="headings">
-            <Img fixed={data.head2.childImageSharp.fixed} />
-            <h2>Our Speakers</h2>
-            <p>
-              Stay tuned for more awesome speakers announcements soon. You can
-              also{' '}
-              <a
-                href="https://checkout.eventlama.com/#/events/reacteurope-2020/cfp"
-                target="_blank"
-              >
-                submit a proposal to our call for paper
-              </a>
-              .
-            </p>
-          </div>
-          <div class="speaker_profile">
-            <div class="row">
-              {event.speakers.map(speaker => {
-                return (
-                  <div class="col-md-3 col-sm-4 col-xs-12">
-                    <div class="speaker_box left_box">
-                      <div class="profile_image">
-                        <Img fluid={speaker.localFile.childImageSharp.fluid} />
-                      </div>
-                      <div class="profile_contnet">
-                        <h3 class="speaker-name">{speaker.name}</h3>
-                        <p class="speaker-bio" bio-full={speaker.bio}>
-                          {speaker.shortBio}
-                        </p>
-                        <ul>
-                          <li>
-                            <a
-                              href={`https://twitter.com/${speaker.twitter}`}
-                              class="icon-social-button"
-                            >
-                              <i class="fa fa-twitter icon-twitter"></i>
-                              <span />
-                            </a>
-                          </li>
-                          <li>
-                            <a
-                              href={`https://github.com/${speaker.github}`}
-                              class="icon-social-button"
-                            >
-                              <i class="fa fa-github icon-github"></i>
-                              <span />
-                            </a>
-                          </li>
-                          {speaker.url ? (
-                            <li>
-                              <a
-                                href="https://ivesvh.com/"
-                                class="icon-social-button"
-                              >
-                                <i class="fa fa-link icon-link"></i>
-                              </a>
-                            </li>
-                          ) : null}
-                        </ul>
-                      </div>
-                      <a
-                        href="#"
-                        class="add_icon show-speaker"
-                        data-toggle="modal"
-                        data-target="#speaker_popup"
-                        onClick={e => {
-                          e.preventDefault()
-                          setShow(true)
-                          setSpeakerProps(speaker)
-                        }}
-                      >
-                        +
-                      </a>
-                    </div>
-                  </div>
-                )
-              })}
-
-              <div class="col-md-3 col-sm-4 col-xs-12">
-                <div class="speaker_box right_box">
-                  <div class="profile_image">
-                    <Img
-                      fluid={data.speaker_placeholder.childImageSharp.fluid}
-                    />
-                  </div>
-                  <div class="profile_contnet">
-                    <h3 class="speaker-name">You?</h3>
-                    <p class="speaker-bio">
-                      Stay tuned for more awesome speakers or{' '}
-                      <a
-                        href="https://checkout.eventlama.com/#/events/reacteurope-2020/cfp"
-                        target="_blank"
-                      >
-                        submit a proposal to our CFP!
-                      </a>
-                      <br />
-                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSejydjRdhyxE5sbzRqT93aHhx0PosforW88yZdem7HejNl-yA/viewform">
-                        Subscribe here to not miss new announcements
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="get_your_ticket">
-              <a href="#tickets">Get Your Ticket</a>
-            </div>
-          </div>
-        </div>
-        <Modal show={show} onHide={() => setShow(false)} id="speaker_popup">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button
-                  type="button"
-                  class="close"
-                  data-dismiss="modal"
-                  onClick={() => setShow(false)}
-                >
-                  &times;
-                </button>
-              </div>
-              <div class="modal-body">
-                <div class="icon_box">
-                  <ul>
-                    <li>
-                      <a
-                        href={`https://twitter.com/${speakerProps.twitter}`}
-                        class="speaker-twitter-modal"
-                      >
-                        <Img fixed={data.pt.childImageSharp.fixed} />
-                      </a>
-                      <a
-                        href={`https://github.com/${speakerProps.github}`}
-                        class="speaker-github-modal"
-                      >
-                        <Img fixed={data.pi.childImageSharp.fixed} />
-                      </a>
-                      <a href={speakerProps.url} class="speaker-url-modal">
-                        <Img fixed={data.pl.childImageSharp.fixed} />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div class="speaker_popup_profile">
-                  <div style={{ width: 200, margin: '0 auto' }}>
-                    <Img fluid={speakerProps.localFile.childImageSharp.fluid} />
-                  </div>
-                  <div class="popup_profile_content">
-                    <h3 class="speaker-name-modal">{speakerProps.name}</h3>
-                    <h5 class="speaker-bio-modal">{speakerProps.shortBio}</h5>
-                    <div class="speaker-bio-full-modal">
-                      <ReactMarkdown source={speakerProps.bio} />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Modal>
-      </section>
+      <SpeakersSection speakers={event.speakers} />
 
       <ScheduleSection
         schedule={schedule}
@@ -1715,41 +1539,6 @@ export const pageQuery = graphql`
       childImageSharp {
         fixed(width: 300) {
           ...GatsbyImageSharpFixed
-        }
-      }
-    }
-    head2: file(relativePath: { eq: "head-2.png" }) {
-      childImageSharp {
-        fixed(width: 80, height: 80) {
-          ...GatsbyImageSharpFixed_withWebp
-        }
-      }
-    }
-    speaker_placeholder: file(relativePath: { eq: "you-round.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-    pt: file(relativePath: { eq: "pt.png" }) {
-      childImageSharp {
-        fixed(width: 32, height: 32) {
-          ...GatsbyImageSharpFixed_withWebp
-        }
-      }
-    }
-    pi: file(relativePath: { eq: "pi.png" }) {
-      childImageSharp {
-        fixed(width: 32, height: 32) {
-          ...GatsbyImageSharpFixed_withWebp
-        }
-      }
-    }
-    pl: file(relativePath: { eq: "pl.png" }) {
-      childImageSharp {
-        fixed(width: 32, height: 32) {
-          ...GatsbyImageSharpFixed_withWebp
         }
       }
     }
