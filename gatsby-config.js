adb7f9b615f1a48c53e562cca843ec977f44fa18
Patrick Aljord <patcito@gmail.com>
2020-02-08 21:48:09 +0100

    add css purge, shrink hero videos

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,5 +108,24 @@ module.exports = {
     'gatsby-plugin-sitemap',
     'gatsby-plugin-twitter',
     'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        whitelist: [
+          '.headroom',
+          '.headroom--unfixed header',
+          '.headroom--unfixed header.desktop_header',
+          '.headroom--unfixed header.desktop_header',
+          'header.fixed-header',
+          'header.fixed-header a.nav-link',
+          'header.fixed-header li.nav-item.active a.nav-link',
+        ], // Don't remove this selector
+        ignore: ['style.scss'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      },
+    },
   ],
 }
