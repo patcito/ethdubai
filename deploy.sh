e2d77a076f7f2a82d70f79153cbef2ccaf67fc53
Patrick Aljord <patcito@gmail.com>
2020-09-14 18:45:19 +0200

    remove deploy.sh

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#rm -rf public/; rm -rf .cache; yarn run gatsby build; scp -r public/* app@eventlama.com:/var/eventlama/data/themes/75
-yarn run gatsby build; scp -r public/* app@eventlama.com:/var/eventlama/data/themes/75
