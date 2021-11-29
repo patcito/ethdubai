377f47e6ebec87d3a5e9baa937be862376461cbf
Patrick Aljord <patcito@gmail.com>
2021-11-29 14:17:34 +0400

    latest

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#rm -rf public/; rm -rf .cache; yarn run gatsby build; scp -r public/* app@eventlama.com:/var/eventlama/data/themes/75
-#yarn run gatsby build; scp -r public/* app@eventlama.com:/var/eventlama/data/themes/88
-rm -rf public/
-yarn run gatsby build
-tar -czvf public.tar.gz public
-scp -r public.tar.gz app@eventlama.com:/var/eventlama/data/themes/88
-ssh app@eventlama.com 'cd /var/eventlama/data/themes/88 && tar xf public.tar.gz && cp -R public/* .'
