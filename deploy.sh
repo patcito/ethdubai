3b2c140f7f8fc570e4394c78a59fd92c5606316d
Patrick Aljord <patcito@gmail.com>
2021-12-02 16:45:35 +0400

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -rf public/
 yarn run gatsby build
 tar -czvf public.tar.gz public
 scp -r public.tar.gz app@eventlama.com:/var/eventlama/data/themes/88
-ssh app@eventlama.com 'cd /var/eventlama/data/themes/88 && tar xf public.tar.gz && cp -R public/* . && rm public.tar.gz && mv public /tmp/oldpublic'
+ssh app@eventlama.com 'cd /var/eventlama/data/themes/88 && tar xf public.tar.gz && cp -R public/* . && rm public.tar.gz && rm -rvf public'
