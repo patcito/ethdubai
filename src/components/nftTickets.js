fea155fe5d925ad50356687cdae594ba16e59c42
Patrick Aljord <patcito@gmail.com>
2021-12-08 17:15:05 +0400

    fail safe svg

--- src/components/nftTickets.js
+++ src/components/nftTickets.js
@@ -436,7 +436,7 @@ export default function NFTTicketsSection() {
       },
     },
   ]
-  const networks = networksTest.slice(0, 5)
+  const networks = networksTest //.slice(0, 5)
   console.log(abi)
   const PUB_KEY =
     '01e32ab579d8a368f879b67a8487bd65093dc6c750a2418c169a146579486f68e08965eab5b00d7dc7349a1374bd9866c895f8997ffdb1d667d143bc555b7854'
@@ -1587,7 +1587,11 @@ export default function NFTTicketsSection() {
         setOwnerIds(ownedids)
         setSuccessPurchase(true)
         try {
-          await getSvgTickets(ownedids)
+          try {
+            await getSvgTickets(ownedids)
+          } catch (error) {
+            console.log('failed svg', error)
+          }
           setShowCheckout(true)
           setSuccessPurchase(true)
           finalPdfTickets = finalPdfTickets.map((ft) => {
