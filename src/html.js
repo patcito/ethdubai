98392f8161abd169d3cec156b71026d188abf94c
Patrick Aljord <patcito@gmail.com>
2022-02-19 20:50:49 +0400

    add pixel

--- src/html.js
+++ src/html.js
@@ -77,6 +77,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->`,
             }}
           />
+          <span
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '436995354447065');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=436995354447065&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+`,
+            }}
+          />
         </head>
         <body>
           <span
